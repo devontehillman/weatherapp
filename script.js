a8978eadb1888d9fb494e01ee9101fc5802f1295
Devonte Hillman <hillmand@mail.gvsu.edu>
2021-01-17 13:00:05 -0500

    dynamicly updating the humidity and temp

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const temp = Array.from(document.querySelectorAll('.temp'));
+const humi = Array.from(document.querySelectorAll('.humi'));
+
+
 // api key for weather api 129a08d8024726f96ac0562f98394ae9
 // var city = userInput
 // var APIkey = "129a08d8024726f96ac0562f98394ae9"
@@ -84,13 +88,20 @@ return dataToReturn;
   $("#btn").click(function(){
     event.preventDefault();
     let userInput = $(".userCity").val()
-    $('#mycity').text(userInput)
+    $('.mycity').text(userInput)
 
      var finalURL = composeURL(userInput)//getsURL
     var myarr = dailyWeatherinfo(finalURL)//gets lat and lon
     var newURl = dailyURL(myarr) //gets URL for daily forecast
     var weatherHumidity = Weatherinfo(newURl)//pass into ajax now we need to return val
     console.log(weatherHumidity)
+    
+    var j=5
+for (let i = 0; i < 5; i++) {
+    temp[i].innerHTML = weatherHumidity[i];
+    humi[i].innerHTML = weatherHumidity[j];
+    j++;
+}
   })
 
 
